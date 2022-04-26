f143aea6d9d0aceb8743ebd9f5304b052cc8b34b
kamruzzamanripon <rkamruzzaman@gmail.com>
2022-04-27 02:16:30 +0600

    make it better 4

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const ecerciseSchema = new Schema({
   userId:{type: String, required:true},
   description: String,
   duration: Number,
-  Date: Date
+  date: Date,
 });
 const Exercise = mongoose.model("Exercise", ecerciseSchema)
 
@@ -42,62 +42,63 @@ app.post("/api/users", (req, res)=>{
 })
 
 
-app.post("/api/users/:_id/exercises", (req, res)=>{
- //return res.json({"data": req.body})
-  const {_id:userId, description, duration, date} = req.body;
-  //return res.json({"data": userId})
-  if(!date){
-    date = new Date();
-  }
-
-  People.findById(userId, (err,data)=>{
-    if(!data){
-      res.send("Unknown Id")
-    }else{
-      const username = data.username;
-      const newExercise = new Exercise({userId, description, duration, date})
-      newExercise.save((err,data)=>{
-        //res.json({userId, username, description, data})
-        let dateFormatted = new Date(date).toDateString();
-        res.json({username, description, duration: +duration, _id:userId, date: dateFormatted})
-      })
-    }
-  })
-  //res.json({"request": req.body})
-})
-
 // app.post("/api/users/:_id/exercises", (req, res)=>{
-//   const {_id:id} = req.params;
-//   const {description, duration, date} =req.body;
-//   People.findById(id, (err, userData)=>{
-//     if(err || !userData){
-//       res.send("Could not find user")
+//  //return res.json({"data": req.body})
+//   const {_id:userId, description, duration, date} = req.body;
+//   let payload = req.body;
+//   //return res.json({"data": req.body})
+//   if(!date){
+//     date = new Date();
+//   }
+
+//   People.findById(userId, (err,data)=>{
+//     if(!data){
+//       res.send("Unknown Id")
 //     }else{
-//       const newExercise = new Exercise({
-//         userId: id,
-//         description,
-//         duration,
-//         date: new Date(date)
-//       })
-//       newExercise.save((err, data)=>{
-//         if(err || !data){
-//           res.send("There was an error saving this exercise")
-//         }else{
-//           const {description, duration, date, _id} = data;
-//           //return res.send({"data": data})
-//           //let dateFormatted = new Date(date).toDateString();
-//           res.json({
-//             username: userData.username,
-//             description,
-//             duration, 
-//             date: date.toDateString(),
-//             _id: userData.id
-//           })
-//         }
+//       const username = data.username;
+//       const newExercise = new Exercise({userId, description, duration, date})
+//       newExercise.save((err,data)=>{
+//         //res.json({userId, username, description, data})
+//         let dateFormatted = new Date(date).toDateString();
+//         res.json({username, description, duration: +duration, _id:userId, date: dateFormatted})
 //       })
 //     }
 //   })
+//   //res.json({"request": req.body})
 // })
+
+app.post("/api/users/:_id/exercises", (req, res)=>{
+  const {_id:id} = req.params;
+  const {description, duration, date} =req.body;
+  People.findById(id, (err, userData)=>{
+    if(err || !userData){
+      res.send("Could not find user")
+    }else{
+      const newExercise = new Exercise({
+        userId: id,
+        description,
+        duration,
+        date: new Date(date)
+      })
+      newExercise.save((err, data)=>{
+        if(err || !data){
+          res.send("There was an error saving this exercise")
+        }else{
+          const {description, duration, date, _id} = data;
+          //return res.send({"data": data})
+          let dateFormatted = new Date(date).toDateString();
+          res.json({
+            username: userData.username,
+            description,
+            duration, 
+            date: date.toDateString(),
+            _id: userData.id
+          })
+        }
+      })
+    }
+  })
+})
 
 
 app.get("/api/users/:_id/logs", (req, res)=>{
@@ -132,7 +133,7 @@ app.get("/api/users/:_id/logs", (req, res)=>{
           const log = rawLog.map((l)=>({
             description: l.description,
             duration: l.duration,
-            date: l.date.toDateString()
+            date: l.date
           }))
           res.json({username, count, _id, log})
         }
